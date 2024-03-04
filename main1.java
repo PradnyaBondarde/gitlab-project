f37d14799932c2318e318e6899201d5f17e276a6
Pradnya Bondarde <142186818+PradnyaBondarde@users.noreply.github.com>
2024-03-04 10:54:46 +0530

    main1.java in TMS

---
+++ main1.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        TaskManager taskManager = new TaskManager();
+        Scanner scanner = new Scanner(System.in);
+
+        while(true) {
+            System.out.println("1. Add task");
+            System.out.println("2. Edit task");
+            System.out.println("3. Delete task");
+            System.out.println("4. Display tasks");
+            System.out.println("5. Sort tasks by progress");
+            System.out.println("6. Exit");
+            System.out.print("Enter your choice: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine();  // consume newline
+//switch statement
+}
+}
+}
